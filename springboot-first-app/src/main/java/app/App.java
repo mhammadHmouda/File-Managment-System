8531a9ea3685e4ece0ccb7b68cb7e61fc06ccb97
Ahmad Omar <ahmadomarns@gmail.com>
2022-12-18 01:28:21 +0200

    Creating user Entity With Auth SignIn and SignUp

--- springboot-first-app/src/main/java/app/App.java
+++ springboot-first-app/src/main/java/app/App.java
@@ -1,14 +1,18 @@
-package filerepository;
-
+package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+
 public class App {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(App.class, args);
+
 
 	}
 
