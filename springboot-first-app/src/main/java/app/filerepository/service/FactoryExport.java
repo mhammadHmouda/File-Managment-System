5d7730482f6f0aed7f732a130b0d5ebaec5fd117
deemashahawan <dshahwan42@gmail.com>
2022-12-18 19:48:07 +0000

    add classification operations

---
+++ springboot-first-app/src/main/java/app/filerepository/service/FactoryExport.java
@@ -0,0 +1,22 @@
+package app.filerepository.service;
+
+import app.filerepository.model.DBFile;
+import app.filerepository.repository.DBFileRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FactoryExport {
+    public static List<DBFile>  factory(String val, String name, DBFileRepository dbFileRepository){
+        if(name.equals("fileName")){
+            return dbFileRepository.findByName(val);
+        }
+        else if(name.equals("fileType")){
+            return dbFileRepository.findByType(val);
+        }
+        else if(name.equals("size")){
+            return dbFileRepository.findBySize(val);
+        }
+     return new ArrayList<>();
+    }
+}
