2c058eb84a6e2cc920fbd2fef8153d8aa44d5dcb
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2022-12-25 18:49:03 +0200

    Add Encryption component

---
+++ springboot-first-app/src/main/java/app/encryption/EncryptionService.java
@@ -0,0 +1,49 @@
+package app.encryption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+@Service
+public class EncryptionService {
+    private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
+    private static final int IV_LENGTH = 16;
+    private static SecretKey secretKey;
+    private static byte[] iv;
+
+    @Autowired
+    public EncryptionService() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        secretKey = keyGenerator.generateKey();
+        iv = createIv();
+    }
+
+    public static byte[] encrypt(byte[] content) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
+        return cipher.doFinal(content);
+    }
+
+    public static byte[] decrypt(byte[] content) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+        return cipher.doFinal(content);
+    }
+
+    private static byte[] createIv() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[IV_LENGTH];
+        secureRandom.nextBytes(iv);
+        return iv;
+    }
+}
+
