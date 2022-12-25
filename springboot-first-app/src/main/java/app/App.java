7147129322a06656a0d3607a196d4b968477f2e7
Hamo0od <hmoudahamood@gmail.com>
2022-12-25 12:01:56 +0200

    Add exclude auto security

--- springboot-first-app/src/main/java/app/App.java
+++ springboot-first-app/src/main/java/app/App.java
@@ -1,10 +1,11 @@
 package app;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication
-
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class App {
 
 	public static void main(String[] args) {
